1c9f554d02ef8c791115cf81a1de35dd8380209c
DAM <victoramadorsanjuan@gmail.com>
2016-09-15 20:48:29 +0000

    Modificacion coches añadidos

--- src/main.java
+++ src/main.java
@@ -14,13 +14,17 @@ public class main {
         coche jaguar = new coche("2534HJG", "Jaguar", "F-type", 80000.0, "rojo");
         coche audi = new coche("1324GJK", "Audi", "R8", 810000.0, "azul");
         coche mercedes = new coche("2342KJH", "Mercedes", "a45 AMG", 800400.0, "negro");
+        coche iepe = new coche("2342KJH", "Mercedes", "a45 AMG", 800400.0, "negro");
 
         cochesList.add(jaguar);
         cochesList.add(audi);
         cochesList.add(mercedes);
+        cochesList.add(iepe);
 
         System.out.println("Muestro el jaguar " + cochesList.get(0));
-
+        System.out.println("Muestro el audi " + cochesList.get(1));
+        System.out.println("Muestro el mercedes " + cochesList.get(2));
+        System.out.println("Muestro el iepe " + cochesList.get(3));
 
 
 
